0e1ec80cc2c0f2da044bb07b4a18131bb48a7845
Diogo Alves da Cunha Ferreira <diogoa42@gmail.com>
2017-04-12 23:49:34 -0300

    - Validações de Campos Obrigatórios - Mensagem de Sucesso ao enviar a mensagem corretamente - padronização dos métodos

--- app/scripts/controllers/contato.controller.js
+++ app/scripts/controllers/contato.controller.js
@@ -8,34 +8,62 @@
  * Controller of the myApp
  */
 angular.module('myApp')
-  .controller('ContatoController', function (firebase) {
+  .controller('ContatoController', function ($scope, firebase) {
     var vm = this;
     vm.contatoFeito = false;
     vm.erroMensagemDeContato = false;
     
      var limparCampos = function(){
+         $scope.$apply(function(){
             vm.contatoFeito = true;
             vm.Nome = "";
             vm.Email = "";
             vm.Assunto = "";
-            vm.Mensagem = "";            
+            vm.Mensagem = "";
+         });       
+     };
+     var mensagensDeErro = function(){
+        $scope.$apply(function(){
+            vm.erroMensagemDeContato = true;
+        });
      };
 
      vm.enviarMensagem = function () {
+
+         vm.campoNomeNaoPreenchido = false;
+         vm.campoEmailNaoPreenchido = false;
+         //vm.campoEmailInvalido = false;
+         vm.campoMensagemNaoPreenchido = false;
+
+         if(!vm.Nome)
+         {
+             vm.campoNomeNaoPreenchido = true;
+             return;
+         }
+         if(!vm.Email)
+         {
+             vm.campoEmailNaoPreenchido = true;
+             return;
+         }
+         if(!vm.Mensagem)
+         {
+             vm.campoMensagemNaoPreenchido = true;
+             return;
+         }
            var data = {
                nome: vm.Nome,
                email: vm.Email,
                assunto: vm.Assunto ? vm.Assunto : "",
                mensagem: vm.Mensagem,
            };
-           vm.teste = data;
+           
            if (vm.Nome && vm.Email && vm.Mensagem) {
                firebase.database().ref().child('Contato').push(data)
                    .then(function () {
                         limparCampos();                       
                    })
                    .catch(function () {
-                       vm.erroMensagemDeContato = true;
+                       mensagensDeErro();
                    });
            }
         };
